f10548492ed18a9b900db269c247e1e9636ccb11
Howard Kennedy <hkennedyvi@gmail.com>
2020-01-20 14:15:57 -0800

    Routes adjusted

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,23 +2,62 @@
 // DEPENDENCIES
 // We need to include the path package to get the correct file path for our html
 // ===============================================================================
-var path = require("path");
-
+const path = require("path");
+const notes = [
+    {id: 1, name: 'note1'},
+    {id: 2, name: 'note2'},
+    {id: 3, name: 'note3'}
+];
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function (app) {
-
+    
     app.get('/api/notes', (req, res) => {
         res.json(notes);
     });
-
+    
     app.get('/api/notes/:id', (req, res) => {
         const note = notes.find(c => c.id === parseInt(req.params.id));
         if (!note) res.status(404).send("The note with the given ID not found");
         res.json(note);
+    });
+    
+    app.post('/api/notes', (req, res) => {
+        const newNote = {
+            id: notes.length + 1,
+            name: req.body.name,
+        };
+        notes.push(newNote);
+        res.send(newNote);
+    });
+    
+    //Update
+    app.put('/api/notes/:id', (req, res) => {
+        //Look up the note
+        //If not existing, return 404
+        const note = notes.find(c => c.id === parseInt(req.params.id));
+        if (!note) res.status(404).send("The note with the given ID not found");
+    
+    res.send(note);
+    
+    });
+    
+    app.delete('/api/notes/:id', (req, res) => {
+        //Look up the note
+        //Not existing, send error
+        const note = notes.find(c => c.id === parseInt(req.params.id));
+        if (!note) res.status(404).send("The note with the given ID not found");
+    
+        //Delete
+        const index = notes.indexOf(note);
+        notes.splice(index, 1);
+    
+        //Return response
+        res.send(note);
+    
     });
 
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@
 // DEPENDENCIES
 // We need to include the path package to get the correct file path for our html
 // ===============================================================================
-var path = require("path");
+const path = require("path");
 
 
 // ===============================================================================
@@ -22,6 +22,8 @@ module.exports = function (app) {
     app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
+
+   
     // If no matching route is found default to home
     // app.get("*", function (req, res) {
     //     res.sendFile(path.join(__dirname, "../public/home.html"));
