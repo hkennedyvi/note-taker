1e1ab3d6fba53498e076c394df9199e77b34318a
Howard Kennedy <hkennedyvi@gmail.com>
2020-01-21 21:57:50 -0800

    Paths fixed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 2000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
